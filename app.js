54c71689671b017fe39c6932b9f3464d7a847698
Johan Edenfjord <johan@edenfjord.se>
2020-08-24 15:19:47 +0200

    first of many updates
    What we did in class today

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 
+const peerOnOpen = (id) => {
+    document.querySelector('.my-peer-id').innerHTML = id;
+};
+
+const myPeerID = location.hash.slice(1);
+
+console.log(myPeerID);
+
+let peer = new Peer(myPeerID, {
+    host: "glajan.com",
+    port: 8443, 
+    path: '/myapp', 
+    secure: true,
+});
+
+peer.on('open', peerOnOpen);
+
