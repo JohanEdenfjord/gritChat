c75548365239d8a3e4b0868e7eb37df601a49aa3
Johan Edenfjord <johan@edenfjord.se>
2020-08-25 11:09:41 +0200

    Update app.js
    innan Lunch update

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
 
+//if there is a # in the url this puts the myPeerId where it should be!
 const peerOnOpen = (id) => {
     document.querySelector('.my-peer-id').innerHTML = id;
 };
 
+//adds an id by putting an # after the URL.
 const myPeerID = location.hash.slice(1);
 
-console.log(myPeerID);
-
-let peer = new Peer(myPeerID, {
+const peerOnError = (error) => {
+    console.log(error)
+}
+// Connecting to the peer-server!
+const peer = new Peer(myPeerID, {
     host: "glajan.com",
     port: 8443, 
     path: '/myapp', 
     secure: true,
 });
 
+//open the connection! and handles eventual error!
 peer.on('open', peerOnOpen);
+peer.on('error', peerOnError);
 
+document.querySelector('.list-all-peers-button').addEventListener('click', () => {
+
+    peer.listAllPeers((peers) => {
+
+        const peersEl = document.querySelector('.peers');        
+        const ul = document.createElement('ul');
+
+        peers
+        .filter(
+            (p) => p !== myPeerID) //tenerary
+        .forEach(peerID => {
+            const li = document.createElement('li');
+
+            li.innerText = peerID
+            ul.appendChild(li)
+        });
+
+        peersEl.appendChild(ul)
+    });
+});
