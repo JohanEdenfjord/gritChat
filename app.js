eb6d878eff5ffaa1f1cbb0bbae529073165c11da
Johan Edenfjord <johan@edenfjord.se>
2020-09-07 11:47:17 +0200

    Update app.js
    new stuff. including local mediastream

--- app.js
+++ app.js
@@ -1,38 +1,36 @@
 //self invoked anonymous function!
-(function (){
+(function () {
+  //global variable
+  let peer = null;
+  let conn = null;
+  let messageBox = document.querySelector(".new-message");
 
-//global variable
-let peer = null;
-let conn = null;
-let messageBox = document.querySelector(".new-message");
+  //if there is a # in the url this puts the myPeerId where it should be!
+  const peerOnOpen = (id) => {
+    document.querySelector(".my-peer-id").innerHTML = id;
+  };
 
+  //adds an id by putting an # after the URL.
+  let myPeerID = location.hash.slice(1);
 
-//if there is a # in the url this puts the myPeerId where it should be!
-const peerOnOpen = (id) => {
-    document.querySelector('.my-peer-id').innerHTML = id;
-};
-
-//adds an id by putting an # after the URL.
-let myPeerID = location.hash.slice(1);
-
-const peerOnError = (error) => {
+  const peerOnError = (error) => {
     console.log(error);
-};
+  };
 
-let consoleLog = (e) => {
-    console.log(e)
-};
+  let consoleLog = (e) => {
+    console.log(e);
+  };
 
-// Connections
-peer = new Peer(myPeerID, {
+  // Connections
+  peer = new Peer(myPeerID, {
     host: "glajan.com",
     port: 8443,
     path: "/myapp",
     secure: true,
     config: {
       iceServers: [
-        { 
-            url: ["stun:eu-turn7.xirsys.com"] 
+        {
+          url: ["stun:eu-turn7.xirsys.com"],
         },
         {
           username:
@@ -45,64 +43,69 @@ peer = new Peer(myPeerID, {
   });
 
   function printMessage(message, who) {
-      const messageDiv = document.querySelector(".messages");
-      const messageWrapper = document.createElement('div');
-      const newMessageDiv = document.createElement('div');
-      let timestamp = new Date().toLocaleTimeString();  
-      newMessageDiv.innerText = (message + "\n" + timestamp);
-      //console.log(message);
-      messageWrapper.classList.add("message");
-      if (who === "me"){
-          messageWrapper.classList.add("me");
-          //console.log("me");
-      } else if (who === "them"){
-          messageWrapper.classList.add("them");
-          //console.log("them");
-      }
-      messageWrapper.appendChild(newMessageDiv);
-      messageDiv.appendChild(messageWrapper);
-  };
-  
-  document.querySelector(".send-new-message-button").addEventListener("click", () => {      
+    const messageDiv = document.querySelector(".messages");
+    const messageWrapper = document.createElement("div");
+    const newMessageDiv = document.createElement("div");
+    let timestamp = new Date().toLocaleTimeString();
+    newMessageDiv.innerText = message + "\n" + timestamp;
+    //console.log(message);
+    messageWrapper.classList.add("message");
+    if (who === "me") {
+      messageWrapper.classList.add("me");
+      //console.log("me");
+    } else if (who === "them") {
+      messageWrapper.classList.add("them");
+      //console.log("them");
+    }
+    messageWrapper.appendChild(newMessageDiv);
+    messageDiv.appendChild(messageWrapper);
+    messageDiv.scrollTo(0, messageDiv.scrollHeight);
+  }
+
+  document
+    .querySelector(".send-new-message-button")
+    .addEventListener("click", () => {
       sendTheMessage();
     });
 
-    messageBox.addEventListener("keyup", (e) => {
-        if(e.keyCode === 13){
-            sendTheMessage();
-        }
-    });
+  messageBox.addEventListener("keyup", (e) => {
+    if (e.key === "Enter") {
+      sendTheMessage();
+    }
+  });
 
-    function sendTheMessage() {
-        let message = document.querySelector(".new-message").value;
-        conn.send(message);
-        printMessage(message, "me");
-        messageBox.value= "";
-    };
-const connectToPeerClick = (el) => {         
-    const peerId = el.target.textContent;  
+  function sendTheMessage() {
+    let message = document.querySelector(".new-message").value;
+    conn.send(message);
+    printMessage(message, "me");
+    messageBox.value = "";
+  }
+
+  const connectToPeerClick = (el) => {
+    const peerId = el.target.textContent;
     conn && conn.close();
-    console.log("Atempting to Connect to " + peerId)   
+    console.log("Atempting to Connect to " + peerId);
     conn = peer.connect(peerId);
 
-    conn.on('open', () =>{
-        console.log("connection is ....OPEN!")
-        const event = new CustomEvent('peer-changed', {detail: { peerId: peerId }});
-        document.dispatchEvent(event);
-        conn.on("data", (data) => {
-            console.log(data);
-            printMessage(data, "them");
-        });
+    conn.on("open", () => {
+      console.log("connection is ....OPEN!");
+      const event = new CustomEvent("peer-changed", {
+        detail: { peerId: peerId },
+      });
+      document.dispatchEvent(event);
+      conn.on("data", (data) => {
+        console.log(data);
+        printMessage(data, "them");
+      });
     });
-    conn.on('error', consoleLog);
-};
+    conn.on("error", consoleLog);
+  };
 
+  //open the connection! and handles eventual error!
+  peer.on("open", peerOnOpen);
+  peer.on("error", peerOnError);
 
-//open the connection! and handles eventual error!
-peer.on('open', peerOnOpen);
-peer.on('error', peerOnError);
-
-peer.on("connection", (newConnection) => {
+  peer.on("connection", (newConnection) => {
     console.log("connection from remote peer is established!");
     console.log(newConnection);
     //closing previous connection
@@ -113,53 +116,80 @@ peer.on("connection", (newConnection) => {
     conn = newConnection;
 
     //callback for event!
-    conn.on("data",(data) => {
-        console.log(data);
-        printMessage(data, "them")
+    conn.on("data", (data) => {
+      console.log(data);
+      printMessage(data, "them");
     });
 
     //create peerChanged event
-    const peerChangedEvent = new CustomEvent("peer-changed", {detail: {peerId: conn.peer}});
+    const peerChangedEvent = new CustomEvent("peer-changed", {
+      detail: { peerId: conn.peer },
+    });
     document.dispatchEvent(peerChangedEvent);
-});
+  });
 
-//find and select where we will work to list out the other connected people!
-document.querySelector('.list-all-peers-button').addEventListener('click', () => {
-
-    peer.listAllPeers((peers) => {
-
-        const peersEl = document.querySelector('.peers'); //decides where to put the peers-list   
+  //find and select where we will work to list out the other connected people!
+  document
+    .querySelector(".list-all-peers-button")
+    .addEventListener("click", () => {
+      peer.listAllPeers((peers) => {
+        const peersEl = document.querySelector(".peers"); //decides where to put the peers-list
         peersEl.firstChild && peersEl.firstChild.remove();
-        const ul = document.createElement('ul'); //create a unordered list!
+        const ul = document.createElement("ul"); //create a unordered list!
 
         peers
-        .filter(
-            (p) => p !== myPeerID) //tenerary to filter out your own id!
-        .forEach(peerID => {
-            const li = document.createElement('li');
-            const button = document.createElement('button');
+          .filter((p) => p !== myPeerID) //tenerary to filter out your own id!
+          .forEach((peerID) => {
+            const li = document.createElement("li");
+            const button = document.createElement("button");
             button.innerText = peerID; //making buttons of the ID
             button.classList.add("connect-button"); //well..
             button.classList.add(`peerID-${peerID}`); //adding a PeerID to the class
             button.addEventListener("click", connectToPeerClick);
-            li.appendChild(button);//adding the button to the list element
-            ul.appendChild(li);//adding the list-element to the ul.
-        });
+            li.appendChild(button); //adding the button to the list element
+            ul.appendChild(li); //adding the list-element to the ul.
+          });
         peersEl.appendChild(ul); //setting the list to the peers El
+      });
     });
-});
 
-document.addEventListener('peer-changed', (ev) => {
+  //display video of me!
+  navigator.mediaDevices
+    .getUserMedia({ audio: false, video: true })
+    .then((stream) => {
+      const video = document.querySelector(".video-container.me video");
+      video.muted = true;
+      video.srcObject = stream;
+    });
+
+  const startVideoCall = () => {
+    const video = document.querySelector(".video-container.them");
+    const startButton = video.querySelector(".start");
+    const stopButton = video.querySelector(".stop");
+    startButton.classList.remove("active");
+    stopButton.classList.add("active");
+  };
+
+  //start Video click handler!
+  document
+    .querySelector(".video-container.them .start")
+    .addEventListener("click", startVideoCall);
+
+  document.addEventListener("peer-changed", (ev) => {
     const peerId = ev.detail.peerId;
     console.log("peer-changed: ", peerId);
-    const name = document.querySelector(".name");
-    document.querySelectorAll('.connect-button').forEach((el) => {
-        el.classList.remove("connected");
+    document.querySelector(".name").innerHTML = peerId;
+    //removing connected when changing peer!
+    document.querySelectorAll(".connect-button").forEach((el) => {
+      el.classList.remove("connected");
     });
-    name.innerHTML = peerId;
-    const button = document.querySelector(`.connect-button.peerID-${peerId}`);    
-    button.classList.add('connected');    
-});
+    const button = document.querySelector(`.connect-button.peerID-${peerId}`);
+    button.classList.add("connected");
 
+    //video update subtext and so on
+    const video = document.querySelector(".video-container.them");
+    video.classList.add("connected");
+    video.querySelector(".stop").classList.remove("active");
+    video.querySelector(".start").classList.add("active");
+  });
 })();
- 
